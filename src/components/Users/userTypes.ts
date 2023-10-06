b32fb880ea4cec1ce1f18caa3e63a767ab3e21ca
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-10-06 12:45:44 -0300

    feat: add typing to user components

---
+++ src/components/Users/userTypes.ts
@@ -0,0 +1,14 @@
+export interface User {
+	id: number;
+	name: string;
+	lastname: string;
+	gender: string;
+	address: string;
+	phone: number;
+	photo: string;
+	birthday: Date;
+	position: string;
+	admission: Date;
+	sector: string;
+	salary: number;
+}
